4e4be6f05ac20da568b0ddca61ee0e94c9d7fb8c
Jaredare <drakelongtoot@gmail.com>
2022-02-09 16:52:27 -0600

    put a to do list in main

--- __main__.py
+++ __main__.py
@@ -6,6 +6,14 @@ director.start_game()
 from Jumper.director import Skydiver
 MySkydiver = Skydiver()
 
+#TO DO:
+# We need a win and lose condition. I'm thinking we put something just after our word and or guessed letters print before we have them guess a letter to check and see if they won or lost. 
+# If they did, I think we would print a win or lose message and stop the loop.
+
+
+
+
+
 
 # ------ OLD MAIN ------
 
