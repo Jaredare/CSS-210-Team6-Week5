1f87e021eb41327c06d4b77d9d1797bead12d236
Jaredare <drakelongtoot@gmail.com>
2022-02-09 16:54:27 -0600

    Added a little more to the to do list

--- __main__.py
+++ __main__.py
@@ -9,6 +9,7 @@ MySkydiver = Skydiver()
 #TO DO:
 # We need a win and lose condition. I'm thinking we put something just after our word and or guessed letters print before we have them guess a letter to check and see if they won or lost. 
 # If they did, I think we would print a win or lose message and stop the loop.
+# That will be done using the self.parachute_condition variable in director.py
 
 
 
